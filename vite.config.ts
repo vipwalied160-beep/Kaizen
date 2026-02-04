0ecb69a14026d778fec2dda9d0d4acb8c7109ded
Walied Hamdy <vipwalied160@gmail.com>
2026-02-05 00:58:39 +0200

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default defineConfig({
+  base: '/',  // ← أضف السطر ده مهم جدًا لـ Vercel
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
